03514a1601977b114de2e05b9a7fc317ab2243de
Omar Daniel Corahua Valencia <omardanielcv@hotmail.com>
2020-04-25 10:37:23 -0500

    Conexion a la BD

---
+++ proStreamPartido/bin/BDConnection.js
@@ -0,0 +1,9 @@
+﻿const mysql=require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 3,
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'oculto',
+    database: 'bdPryStreamDep'
+});
+module.exports=pool;
--- proStreamPartido/routes/index.js
+++ proStreamPartido/routes/index.js
@@ -1,9 +1,27 @@
+const conn=require('../bin/BDConnection')
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Practica Calificada' });
+  conn.getConnection((err, conexion) => {
+    if(err){
+      console.log('Error de conexion')
+      res.send('Error de conexion')
+    }else{
+      sql = 'SELECT * FROM tEquipo'
+      conn.query(sql,(err, resultado) =>{
+        if(err){
+          console.log('Error de consulta')
+          res.send('Error de consulta')
+        }else{
+          console.log(resultado)
+          res.render('index',{title:'Desde la BD',datos:resultado})
+        }
+      })
+    }
+  })
 });
 router.get('/login', function(req, res, next) {
   res.render('login');
