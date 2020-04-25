26d9879c58cd167d86b5cd083dd5cf5b93ee012c
Omar Daniel Corahua Valencia <omardanielcv@hotmail.com>
2020-04-25 13:22:27 -0500

    Login inicial

--- proStreamPartido/routes/index.js
+++ proStreamPartido/routes/index.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
     }
   })
 });
-router.get('/login', function(req, res, next) {
+router.get('/login', (req, res, next) => {
   res.render('login');
 });
 
